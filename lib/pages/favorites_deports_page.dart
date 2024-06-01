d1f3db431523db640951fc1e44c2abb5f804211b
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-01 12:45:06 -0500

    avance favorite deports

--- lib/pages/favorites_deports_page.dart
+++ lib/pages/favorites_deports_page.dart
@@ -6,6 +6,15 @@ class FavoritesDeportsPage extends StatefulWidget {
 }
 
 class _FavoritesDeportsPageState extends State<FavoritesDeportsPage> {
+  List<Map<String, dynamic>> deportsList = [
+    {"name": "Gimnasio", "isFavorite": false},
+    {"name": "Fútbol", "isFavorite": false},
+    {"name": "Golf", "isFavorite": false},
+    {"name": "Baloncesto", "isFavorite": false},
+    {"name": "Natación", "isFavorite": false},
+    {"name": "Voley", "isFavorite": false},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +23,73 @@ class _FavoritesDeportsPageState extends State<FavoritesDeportsPage> {
         centerTitle: true,
         backgroundColor: Colors.orange,
       ),
-      body: Column(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TituloWidget("¿Cuáles son tus deportes favoritos?"),
+            SizedBox(
+              height: 24,
+            ),
+            Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              alignment: WrapAlignment.center,
+              children: [
+                for (int i = 0; i < deportsList.length; i++)
+                  ItemDeportWidget(deportsList[i]["name"])
+              ],
+            ),
+            Divider(
+              thickness: 3,
+              color: Colors.black,
+              height: 48,
+            ),
+            TituloWidget("Mis deportes favoritos son:")
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TituloWidget extends StatelessWidget {
+  String texto;
+  TituloWidget(this.texto);
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      texto,
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class ItemDeportWidget extends StatelessWidget {
+  String tituloDeporte;
+  ItemDeportWidget(this.tituloDeporte);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          // color: Colors.redAccent,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.orange,
+            width: 2,
+          ),
+        ),
+        child: Text(
+          tituloDeporte,
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
     );
   }
 }
